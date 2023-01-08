f3565ab16f349a5746d313007bfc82519e14aaf9
aruzo <dawid12391111@gmail.com>
2023-01-08 19:12:25 +0100

    Make msw available only in tests (#1)

--- frontend/mocks/handlers.ts
+++ frontend/mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { rest } from "msw";
 
+function getApiUrl(path: string) {
+  return new URL(path, `${process.env.NEXT_PUBLIC_API_URL}`).toString();
+}
+
 const handlers = [
-  rest.get("/api/questions", (req, res, ctx) => {
+  rest.get(getApiUrl("/questions"), (req, res, ctx) => {
     return res(ctx.status(200), ctx.json([]));
   }),
 ];
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,6 @@
 import type { AppProps } from "next/app";
 import GlobalStyle from "styles/global-style";
 
-if (process.env.NEXT_PUBLIC_MOCK_API === "enabled") {
-  require("mocks");
-}
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
