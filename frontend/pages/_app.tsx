0acb07ce9657328f9474a7cf7f67b90bc4cf3988
aruzo <dawid12391111@gmail.com>
2023-01-08 18:32:16 +0100

    Configure styled-components and _document (#1)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
+import GlobalStyle from "styles/global-style";
 
 if (process.env.NEXT_PUBLIC_MOCK_API === "enabled") {
   require("mocks");
 }
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
